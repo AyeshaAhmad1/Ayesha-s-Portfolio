f2a2ba9c8c7f7500feb3da09b8ebaa814f57106c
developerayeshaahmad-netizen <developer.ayeshaahmad@gmail.com>
2026-09-03 08:35:38 +0500

    Update project model

--- Portfolio/Home/models.py
+++ Portfolio/Home/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Project(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=300)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=700)
     image = models.ImageField(
         upload_to= "projects/",
         blank= True,
         null= True
     )
-    tech_stack = models.CharField(max_length=60)
+    tech_stack = models.CharField(max_length=200)
     git_url = models.URLField(max_length=200)
     live_demo = models.URLField(
         max_length=200,    
